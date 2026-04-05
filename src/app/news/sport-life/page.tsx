5b81c7961f928613087d61b8247cb4af35bb57a2
OrdynsDav <davohak2009@gmail.com>
2026-04-05 11:04:50 +0300

    fix: fixed import in news page

--- src/app/news/sport-life/page.tsx
+++ src/app/news/sport-life/page.tsx
@@ -1,6 +1,6 @@
 import { PdfAttachmentsSection } from "@/components/ui/pdfDownloadLink/PdfDownloadLink";
 import { Container } from "@/components/ui";
-import styles from "../page.module.css";
+import styles from "./page.module.css";
 
 export default function NewsSportLifePage() {
   return (
